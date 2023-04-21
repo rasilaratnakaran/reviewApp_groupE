a2ab6cee20576cc8ad32905caa7f875d4d6d5360
Ubuntu <c2035925@c2035925001.54u3mwsnyhyehlbvjgoqlz0zld.fx.internal.cloudapp.net>
2023-04-21 16:27:46 +0000

    About, login, Register added

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -4,4 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'myApp/home.html')
+def about(request):
+    return render(request, 'myApp/about.html')
+def login(request):
+    return render(request, 'myApp/login.html')
+def password(request):
+    return render(request, 'myApp/password.html')
+def profile(request):
+    return render(request, 'myApp/profile.html')
+def reg(request):
+    return render(request, 'myApp/reg.html')
 

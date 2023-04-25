03b253174b84c3211681a3729cdd9e10c848ddb9
rasilaratnakaran <rasilaratnakaran@gmail.com>
2023-04-25 01:22:52 +0000

    user registration updated

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+  
 urlpatterns =[
 path('', views.home, name='myApp-home'),
 path('about', views.about, name='myApp-about'),
+path('contact', views.contact, name='myApp-contact'),
 path('login', views.login, name='myApp-login'),
 path('password', views.password, name='myApp-password'),
 path('profile', views.profile, name='myApp-profile'),
 path('reg', views.reg, name='myApp-reg'),
-]
+ ]
+ 
+urlpatterns += staticfiles_urlpatterns()

c25b0a69b3f7f7657d0304369137c257fe0be1a8
rasilaratnakaran <rasilaratnakaran@gmail.com>
2023-04-26 00:33:18 +0000

    database updated

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -6,9 +6,12 @@ urlpatterns =[
 path('', views.home, name='myApp-home'),
 path('about', views.about, name='myApp-about'),
 path('contact', views.contact, name='myApp-contact'),
-path('login', views.login, name='myApp-login'),
+path('signin', views.signin, name='myApp-signin'),
 path('password', views.password, name='myApp-password'),
 path('profile', views.profile, name='myApp-profile'),
+path('smartwatch', views.smartwatch, name='myApp-smartwatch'),
+path('smartphone', views.smartphone, name='myApp-smartphone'),
+path('television', views.television, name='myApp-television'),
 path('reg', views.reg, name='myApp-reg'),
  ]
  
